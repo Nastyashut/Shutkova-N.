405653c4573755a05d675b9bf4cb5e61beff5ceb
Nastyashut <itsmycity75@gmail.com>
2024-11-30 22:23:22 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,55 @@
+import os 
+import signal
+
+N = 27
+L = 100
+pipes = []
+pids = []
+
+for i in range(N):
+    pipes.append(os.pipe())
+    pid = os.fork()
+
+    if pid == 0:
+        # child 
+        break
+    else:
+        pids.append(pid)
+else:
+    for p in pipes:
+        os.close(p[0])
+    while s:=input():
+        s = s.split()
+        c = ord(s[1][0]) % N
+        if s[0] == 'load':
+            os.write(pipes[c][1], ((L - len(s[1]))* ' ' + s[1]).encode())
+        elif s[0] == 'get':
+            s = ' '.join(s)
+            os.write(pipes[c][1], ((L - len(s)) * ' ' + s).encode())
+        elif s[0] == 'kill':
+            for p in pids:
+                os.kill(p, signal.SIGTERM)
+            exit()
+        else:   
+            raise Exception('Incorrect command')
+    exit()
+
+for p in pipes[:-1]:
+    os.close(p[0])
+    os.close(p[1])
+os.close(pipes[-1][1])
+
+reader = os.fdopen(pipes[-1][0])
+words = {}
+
+# child process
+while word := os.read(pipes[-1][0], L).decode().strip():
+    word = word.split()
+    if len(word) == 2:
+        # check 
+        print(words)
+    else:
+        if word[0] in words:
+            words[word[0]] += 1 
+        else:
+            words[word[0]] = 1
